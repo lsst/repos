13568534a5d716dc67659ae402bc6835aabd1f36
Kian-Tat Lim <ktl@slac.stanford.edu>
2020-10-29 10:56:58 -0700

    Replace Travis checks with GitHub Actions.

--- .mdl_style.rb
+++
@@ -1,6 +0,0 @@
-# https://github.com/markdownlint/markdownlint/blob/master/docs/creating_styles.md
-# https://github.com/markdownlint/markdownlint/blob/master/docs/RULES.md
-all
-rule "MD013", :code_blocks => false
-exclude_rule "MD003"
-exclude_rule "MD036"
